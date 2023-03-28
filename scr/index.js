70b2d8967bc0f1eefba7eaadef1a07efd7bb2a14
qqshidiqi <128295964+qqshidiqi@users.noreply.github.com>
2023-03-29 00:26:53 +0800

    Add files via upload

---
+++ scr/index.js
@@ -0,0 +1,9 @@
+
+var tz1=document.querySelector(".card1");
+tz1.onclick=function(){
+    location.href="rw.html";
+}
+var tz2=document.querySelector(".card2");
+tz2.onclick=function(){
+    location.href="cy.html";
+}
---
+++ scr/rw.js
@@ -0,0 +1,41 @@
+
+var tz1=document.querySelector(".zzj");
+tz1.onclick=function(){
+    location.href="rwxqzzj.html";
+}
+var tz2=document.querySelector(".ssm");
+tz2.onclick=function(){
+    location.href="rwxqssm.html";
+}
+var tz3=document.querySelector(".ht");
+tz3.onclick=function(){
+    location.href="rwxqht.html";
+}
+var tz4=document.querySelector(".hfm");
+tz4.onclick=function(){
+    location.href="rwxqhfm.html";
+}
+var tz5=document.querySelector(".df");
+tz5.onclick=function(){
+    location.href="rwxqdf.html";
+}
+var tz6=document.querySelector(".bq");
+tz6.onclick=function(){
+    location.href="rwxqbq.html";
+}
+var tz7=document.querySelector(".wwy");
+tz7.onclick=function(){
+    location.href="rwxqwwy.html";
+}
+var tz8=document.querySelector(".qy");
+tz8.onclick=function(){
+    location.href="rwxqyq.html";
+}
+var tz9=document.querySelector(".wmz");
+tz9.onclick=function(){
+    location.href="rwxqwmz.html";
+}
+var tz10=document.querySelector(".lsz");
+tz10.onclick=function(){
+    location.href="rwxqlsz.html";
+}
